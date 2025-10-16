f00e5bc46adcbde94b8163520af141d9cd94d0b8
sofiaagustinamoral-dot <sofia.agustina.moral@gmail.com>
2025-10-16 14:54:36 -0300

    Agregando cosas

--- src/Vista/PanelPrincipal.java
+++ src/Vista/PanelPrincipal.java
@@ -1,15 +1,21 @@
 package Vista;
 
 import java.awt.Dimension;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JPanel;
 
 
 
 public class PanelPrincipal extends JPanel {
+
+	private JuegoController juegoController;
 	
 	private int ancho;
 	private int alto;
 	private ImagenNave imagenNave;
+
 	
 	
 	
@@ -24,12 +30,29 @@ public class PanelPrincipal extends JPanel {
 		imagenNave= new ImagenNave();
 		add(imagenNave);
 		imagenNave.mover(400,300);
+
+		int posicionNaveJugadorX= 400;
+		int posicionNaveJugadorY= 300;
+		juegoController= new JuegoController(ancho,alto,posicionNaveJugadorX,posicionNaveJugadorY,imagenNave);
 		
 		setFocusable(true);
-		TeclaListener teclaListener = new TeclaListener(imagenNave);
-		addKeyListener(teclaListener);
-		
+		//TeclaListener teclaListener = new TeclaListener();
+		addKeyListener(new KeyAdapter() {
+		@Override
+		public void keyPressed(KeyEvent evento){
+
+			int tecla= evento.getKeyCode();
+			if(tecla ==KeyEvent.VK_RIGHT){
+				juegoController.moverNaveJugadorDerecha();
+
+			}
+		}
+	});
+
+
 	}
-	
+
 
 }
+
+
